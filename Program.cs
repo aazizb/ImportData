303dfce8b82ef75cfc500a926670c481212b7f17
a.a.barry <aazizb@hotmail.com>
2022-06-26 08:33:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //check if input an argument is supplied.
+            if (args is null)
+            {
+                Console.WriteLine("Please enter an argument, and try again.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"Argument is: {args} ");
+            }
+        }
+    }
+}
